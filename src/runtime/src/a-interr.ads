d9481572a4055f9137fc2048bd0d965b3c2c58b9
Anthony <ajxs@panoptic.online>
2019-09-30 08:13:28 +1000

    Runtime interrupt package initial implementation

---
+++ src/runtime/src/a-interr.ads
@@ -0,0 +1,13 @@
+-------------------------------------------------------------------------------
+--  Ada.Interrupts
+--
+--  Purpose:
+--    Contains functionality for handling system interrupts.
+-------------------------------------------------------------------------------
+package Ada.Interrupts is
+   pragma Preelaborate (Ada.Interrupts);
+
+   pragma Implementation_Defined;
+
+   type Interrupt_ID is new Natural;
+end Ada.Interrupts;
---
+++ src/runtime/src/a-intnam.ads
@@ -0,0 +1,28 @@
+-------------------------------------------------------------------------------
+--  Ada.Interrupts.Names
+--
+--  Purpose:
+--    Contains the names for system interrupts.
+-------------------------------------------------------------------------------
+package Ada.Interrupts.Names is
+   pragma Preelaborate (Ada.Interrupts.Names);
+
+   pragma Implementation_Defined;
+
+   IRQ0  : constant Interrupt_ID := 0;
+   IRQ1  : constant Interrupt_ID := 1;
+   IRQ2  : constant Interrupt_ID := 2;
+   IRQ3  : constant Interrupt_ID := 3;
+   IRQ4  : constant Interrupt_ID := 4;
+   IRQ5  : constant Interrupt_ID := 5;
+   IRQ6  : constant Interrupt_ID := 6;
+   IRQ7  : constant Interrupt_ID := 7;
+   IRQ8  : constant Interrupt_ID := 8;
+   IRQ9  : constant Interrupt_ID := 9;
+   IRQ10 : constant Interrupt_ID := 10;
+   IRQ11 : constant Interrupt_ID := 11;
+   IRQ12 : constant Interrupt_ID := 12;
+   IRQ13 : constant Interrupt_ID := 13;
+   IRQ14 : constant Interrupt_ID := 14;
+   IRQ15 : constant Interrupt_ID := 15;
+end Ada.Interrupts.Names;

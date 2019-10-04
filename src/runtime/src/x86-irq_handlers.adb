acf40387dbb8e091352c4ed40074fb536aa88d0a
Anthony <ajxs@panoptic.online>
2019-10-05 00:58:47 +1000

    IRQ handler implementation

--- src/runtime/src/x86-irq_handlers.adb
+++ src/runtime/src/x86-irq_handlers.adb
@@ -19,6 +19,54 @@ package body x86.IRQ_Handlers is
    end IRQ0_Internal_Handler;
 
    ----------------------------------------------------------------------------
+   --  IRQ10_Internal_Handler
+   ----------------------------------------------------------------------------
+   procedure IRQ10_Internal_Handler is
+   begin
+      x86.PIC.Send_EOI (IRQ10);
+   end IRQ10_Internal_Handler;
+
+   ----------------------------------------------------------------------------
+   --  IRQ11_Internal_Handler
+   ----------------------------------------------------------------------------
+   procedure IRQ11_Internal_Handler is
+   begin
+      x86.PIC.Send_EOI (IRQ11);
+   end IRQ11_Internal_Handler;
+
+   ----------------------------------------------------------------------------
+   --  IRQ12_Internal_Handler
+   ----------------------------------------------------------------------------
+   procedure IRQ12_Internal_Handler is
+   begin
+      x86.PIC.Send_EOI (IRQ12);
+   end IRQ12_Internal_Handler;
+
+   ----------------------------------------------------------------------------
+   --  IRQ13_Internal_Handler
+   ----------------------------------------------------------------------------
+   procedure IRQ13_Internal_Handler is
+   begin
+      x86.PIC.Send_EOI (IRQ13);
+   end IRQ13_Internal_Handler;
+
+   ----------------------------------------------------------------------------
+   --  IRQ14_Internal_Handler
+   ----------------------------------------------------------------------------
+   procedure IRQ14_Internal_Handler is
+   begin
+      x86.PIC.Send_EOI (IRQ14);
+   end IRQ14_Internal_Handler;
+
+   ----------------------------------------------------------------------------
+   --  IRQ15_Internal_Handler
+   ----------------------------------------------------------------------------
+   procedure IRQ15_Internal_Handler is
+   begin
+      x86.PIC.Send_EOI (IRQ15);
+   end IRQ15_Internal_Handler;
+
+   ----------------------------------------------------------------------------
    --  IRQ1_Internal_Handler
    ----------------------------------------------------------------------------
    procedure IRQ1_Internal_Handler is
@@ -33,4 +81,79 @@ package body x86.IRQ_Handlers is
       x86.PIC.Send_EOI (IRQ1);
 
    end IRQ1_Internal_Handler;
+
+   ----------------------------------------------------------------------------
+   --  IRQ2_Internal_Handler
+   ----------------------------------------------------------------------------
+   procedure IRQ2_Internal_Handler is
+   begin
+      x86.PIC.Send_EOI (IRQ2);
+   end IRQ2_Internal_Handler;
+
+   ----------------------------------------------------------------------------
+   --  IRQ3_Internal_Handler
+   ----------------------------------------------------------------------------
+   procedure IRQ3_Internal_Handler is
+   begin
+      x86.PIC.Send_EOI (IRQ3);
+   end IRQ3_Internal_Handler;
+
+   ----------------------------------------------------------------------------
+   --  IRQ4_Internal_Handler
+   ----------------------------------------------------------------------------
+   procedure IRQ4_Internal_Handler is
+   begin
+      x86.PIC.Send_EOI (IRQ4);
+   end IRQ4_Internal_Handler;
+
+   ----------------------------------------------------------------------------
+   --  IRQ5_Internal_Handler
+   ----------------------------------------------------------------------------
+   procedure IRQ5_Internal_Handler is
+   begin
+
+      x86.PIC.Send_EOI (IRQ5);
+
+   end IRQ5_Internal_Handler;
+
+   ----------------------------------------------------------------------------
+   --  IRQ6_Internal_Handler
+   ----------------------------------------------------------------------------
+   procedure IRQ6_Internal_Handler is
+   begin
+
+      x86.PIC.Send_EOI (IRQ6);
+
+   end IRQ6_Internal_Handler;
+
+   ----------------------------------------------------------------------------
+   --  IRQ7_Internal_Handler
+   ----------------------------------------------------------------------------
+   procedure IRQ7_Internal_Handler is
+   begin
+
+      x86.PIC.Send_EOI (IRQ7);
+
+   end IRQ7_Internal_Handler;
+
+   ----------------------------------------------------------------------------
+   --  IRQ8_Internal_Handler
+   ----------------------------------------------------------------------------
+   procedure IRQ8_Internal_Handler is
+   begin
+
+      x86.PIC.Send_EOI (IRQ8);
+
+   end IRQ8_Internal_Handler;
+
+   ----------------------------------------------------------------------------
+   --  IRQ9_Internal_Handler
+   ----------------------------------------------------------------------------
+   procedure IRQ9_Internal_Handler is
+   begin
+
+      x86.PIC.Send_EOI (IRQ9);
+
+   end IRQ9_Internal_Handler;
+
 end x86.IRQ_Handlers;
--- src/runtime/src/x86-irq_handlers.ads
+++ src/runtime/src/x86-irq_handlers.ads
@@ -3,6 +3,12 @@
 --
 --  Purpose:
 --    This package contains code for handling individual IRQ requests.
+--    Interrupt request handlers are implemented by wrapping each individual
+--    interrupt handler in an 'entry' function, implemented in assembler. This
+--    'entry function' is responsible for preserving the machine state and
+--    restoring the proper machine state after the interrupt handler has
+--    successfully exited. Refer to the implementation of each 'entry function'
+--    in 'x86-irq_handlers-entry.S'.
 -------------------------------------------------------------------------------
 package x86.IRQ_Handlers is
    pragma Preelaborate (x86.IRQ_Handlers);
@@ -17,7 +23,7 @@ package x86.IRQ_Handlers is
    ----------------------------------------------------------------------------
    procedure IRQ0_Handler
    with Import,
-     Convention => C,
+     Convention    => C,
      External_Name => "__irq0_entry";
 
    ----------------------------------------------------------------------------
@@ -33,12 +39,194 @@ package x86.IRQ_Handlers is
      Convention    => C,
      External_Name => "__irq1_entry";
 
+   ----------------------------------------------------------------------------
+   --  IRQ2_Handler
+   --
+   --  Purpose:
+   --    This function handles the request for IRQ2.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ2_Handler
+   with Import,
+     Convention    => C,
+     External_Name => "__irq2_entry";
+
+   ----------------------------------------------------------------------------
+   --  IRQ3_Handler
+   --
+   --  Purpose:
+   --    This function handles the request for IRQ3.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ3_Handler
+   with Import,
+     Convention    => C,
+     External_Name => "__irq3_entry";
+
+   ----------------------------------------------------------------------------
+   --  IRQ4_Handler
+   --
+   --  Purpose:
+   --    This function handles the request for IRQ4.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ4_Handler
+   with Import,
+     Convention    => C,
+     External_Name => "__irq4_entry";
+
+   ----------------------------------------------------------------------------
+   --  IRQ5_Handler
+   --
+   --  Purpose:
+   --    This function handles the request for IRQ5.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ5_Handler
+   with Import,
+     Convention    => C,
+     External_Name => "__irq5_entry";
+
+   ----------------------------------------------------------------------------
+   --  IRQ6_Handler
+   --
+   --  Purpose:
+   --    This function handles the request for IRQ6.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ6_Handler
+   with Import,
+     Convention    => C,
+     External_Name => "__irq6_entry";
+
+   ----------------------------------------------------------------------------
+   --  IRQ7_Handler
+   --
+   --  Purpose:
+   --    This function handles the request for IRQ7.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ7_Handler
+   with Import,
+     Convention    => C,
+     External_Name => "__irq7_entry";
+
+   ----------------------------------------------------------------------------
+   --  IRQ8_Handler
+   --
+   --  Purpose:
+   --    This function handles the request for IRQ8.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ8_Handler
+   with Import,
+     Convention    => C,
+     External_Name => "__irq8_entry";
+
+   ----------------------------------------------------------------------------
+   --  IRQ9_Handler
+   --
+   --  Purpose:
+   --    This function handles the request for IRQ9.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ9_Handler
+   with Import,
+     Convention    => C,
+     External_Name => "__irq9_entry";
+
+   ----------------------------------------------------------------------------
+   --  IRQ10_Handler
+   --
+   --  Purpose:
+   --    This function handles the request for IRQ10.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ10_Handler
+   with Import,
+     Convention    => C,
+     External_Name => "__irq10_entry";
+
+   ----------------------------------------------------------------------------
+   --  IRQ11_Handler
+   --
+   --  Purpose:
+   --    This function handles the request for IRQ11.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ11_Handler
+   with Import,
+     Convention    => C,
+     External_Name => "__irq11_entry";
+
+   ----------------------------------------------------------------------------
+   --  IRQ12_Handler
+   --
+   --  Purpose:
+   --    This function handles the request for IRQ12.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ12_Handler
+   with Import,
+     Convention    => C,
+     External_Name => "__irq12_entry";
+
+   ----------------------------------------------------------------------------
+   --  IRQ13_Handler
+   --
+   --  Purpose:
+   --    This function handles the request for IRQ13.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ13_Handler
+   with Import,
+     Convention    => C,
+     External_Name => "__irq13_entry";
+
+   ----------------------------------------------------------------------------
+   --  IRQ14_Handler
+   --
+   --  Purpose:
+   --    This function handles the request for IRQ14.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ14_Handler
+   with Import,
+     Convention    => C,
+     External_Name => "__irq14_entry";
+
+   ----------------------------------------------------------------------------
+   --  IRQ15_Handler
+   --
+   --  Purpose:
+   --    This function handles the request for IRQ15.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ15_Handler
+   with Import,
+     Convention    => C,
+     External_Name => "__irq15_entry";
+
 private
    ----------------------------------------------------------------------------
    --  IRQ0_Internal_Handler
    --
    --  Purpose:
-   --    This function contains the Ada code for handling the IRQ0 exception.
+   --    This function contains the Ada code for handling the IRQ0 interrupt.
    --  Exceptions:
    --    None.
    ----------------------------------------------------------------------------
@@ -51,7 +239,7 @@ private
    --  IRQ1_Internal_Handler
    --
    --  Purpose:
-   --    This function contains the Ada code for handling the IRQ1 exception.
+   --    This function contains the Ada code for handling the IRQ1 interrupt.
    --  Exceptions:
    --    None.
    ----------------------------------------------------------------------------
@@ -59,4 +247,187 @@ private
    with Export,
      Convention    => C,
      External_Name => "__irq1_handler";
+
+   ----------------------------------------------------------------------------
+   --  IRQ2_Internal_Handler
+   --
+   --  Purpose:
+   --    This function contains the Ada code for handling the IRQ2 interrupt.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ2_Internal_Handler
+   with Export,
+     Convention    => C,
+     External_Name => "__irq2_handler";
+
+   ----------------------------------------------------------------------------
+   --  IRQ3_Internal_Handler
+   --
+   --  Purpose:
+   --    This function contains the Ada code for handling the IRQ3 interrupt.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ3_Internal_Handler
+   with Export,
+     Convention    => C,
+     External_Name => "__irq3_handler";
+
+   ----------------------------------------------------------------------------
+   --  IRQ4_Internal_Handler
+   --
+   --  Purpose:
+   --    This function contains the Ada code for handling the IRQ4 interrupt.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ4_Internal_Handler
+   with Export,
+     Convention    => C,
+     External_Name => "__irq4_handler";
+
+   ----------------------------------------------------------------------------
+   --  IRQ5_Internal_Handler
+   --
+   --  Purpose:
+   --    This function contains the Ada code for handling the IRQ5 interrupt.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ5_Internal_Handler
+   with Export,
+     Convention    => C,
+     External_Name => "__irq5_handler";
+
+   ----------------------------------------------------------------------------
+   --  IRQ6_Internal_Handler
+   --
+   --  Purpose:
+   --    This function contains the Ada code for handling the IRQ6 interrupt.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ6_Internal_Handler
+   with Export,
+     Convention    => C,
+     External_Name => "__irq6_handler";
+
+   ----------------------------------------------------------------------------
+   --  IRQ7_Internal_Handler
+   --
+   --  Purpose:
+   --    This function contains the Ada code for handling the IRQ7 interrupt.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ7_Internal_Handler
+   with Export,
+     Convention    => C,
+     External_Name => "__irq7_handler";
+
+   ----------------------------------------------------------------------------
+   --  IRQ8_Internal_Handler
+   --
+   --  Purpose:
+   --    This function contains the Ada code for handling the IRQ8 interrupt.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ8_Internal_Handler
+   with Export,
+     Convention    => C,
+     External_Name => "__irq8_handler";
+
+   ----------------------------------------------------------------------------
+   --  IRQ9_Internal_Handler
+   --
+   --  Purpose:
+   --    This function contains the Ada code for handling the IRQ9 interrupt.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ9_Internal_Handler
+   with Export,
+     Convention    => C,
+     External_Name => "__irq9_handler";
+
+   ----------------------------------------------------------------------------
+   --  IRQ10_Internal_Handler
+   --
+   --  Purpose:
+   --    This function contains the Ada code for handling the IRQ10 interrupt.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ10_Internal_Handler
+   with Export,
+     Convention    => C,
+     External_Name => "__irq10_handler";
+
+   ----------------------------------------------------------------------------
+   --  IRQ11_Internal_Handler
+   --
+   --  Purpose:
+   --    This function contains the Ada code for handling the IRQ11 interrupt.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ11_Internal_Handler
+   with Export,
+     Convention    => C,
+     External_Name => "__irq11_handler";
+
+   ----------------------------------------------------------------------------
+   --  IRQ12_Internal_Handler
+   --
+   --  Purpose:
+   --    This function contains the Ada code for handling the IRQ12 interrupt.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ12_Internal_Handler
+   with Export,
+     Convention    => C,
+     External_Name => "__irq12_handler";
+
+   ----------------------------------------------------------------------------
+   --  IRQ13_Internal_Handler
+   --
+   --  Purpose:
+   --    This function contains the Ada code for handling the IRQ13 interrupt.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ13_Internal_Handler
+   with Export,
+     Convention    => C,
+     External_Name => "__irq13_handler";
+
+   ----------------------------------------------------------------------------
+   --  IRQ14_Internal_Handler
+   --
+   --  Purpose:
+   --    This function contains the Ada code for handling the IRQ14 interrupt.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ14_Internal_Handler
+   with Export,
+     Convention    => C,
+     External_Name => "__irq14_handler";
+
+   ----------------------------------------------------------------------------
+   --  IRQ15_Internal_Handler
+   --
+   --  Purpose:
+   --    This function contains the Ada code for handling the IRQ15 interrupt.
+   --  Exceptions:
+   --    None.
+   ----------------------------------------------------------------------------
+   procedure IRQ15_Internal_Handler
+   with Export,
+     Convention    => C,
+     External_Name => "__irq15_handler";
+
 end x86.IRQ_Handlers;

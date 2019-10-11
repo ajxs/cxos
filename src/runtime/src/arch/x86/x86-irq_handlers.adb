7eed364071a1c70ab8d084e577fb6c9170843022
Anthony <ajxs@panoptic.online>
2019-10-11 16:09:41 +1100

    Implemented file license header boilerplate

--- src/runtime/src/arch/x86/x86-irq_handlers.adb
+++ src/runtime/src/arch/x86/x86-irq_handlers.adb
@@ -1,3 +1,14 @@
+-------------------------------------------------------------------------------
+--  Copyright (c) 2019, CXOS.
+--  This program is free software; you can redistribute it and/or modify it
+--  under the terms of the GNU General Public License as published by the
+--  Free Software Foundation; either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  Authors:
+--     Anthony <ajxs [at] panoptic.online>
+-------------------------------------------------------------------------------
+
 with Ada.Interrupts.Names;
 with Interfaces;
 with System.Storage_Elements;
--- src/runtime/src/arch/x86/x86-vga.adb
+++ src/runtime/src/arch/x86/x86-vga.adb
@@ -1,3 +1,14 @@
+-------------------------------------------------------------------------------
+--  Copyright (c) 2019, CXOS.
+--  This program is free software; you can redistribute it and/or modify it
+--  under the terms of the GNU General Public License as published by the
+--  Free Software Foundation; either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  Authors:
+--     Anthony <ajxs [at] panoptic.online>
+-------------------------------------------------------------------------------
+
 package body x86.Vga is
    ----------------------------------------------------------------------------
    --  Clear

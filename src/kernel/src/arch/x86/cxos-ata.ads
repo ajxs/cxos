b4b7b754a3ee21ed0e6750ec8035a57984c044db
Anthony <ajxs@panoptic.online>
2019-12-01 14:17:47 +1100

    ATA driver initial checkin

---
+++ src/kernel/src/arch/x86/cxos-ata.ads
@@ -0,0 +1,22 @@
+-------------------------------------------------------------------------------
+--  Copyright (c) 2019, CXOS.
+--  This program is free software; you can redistribute it and/or modify it
+--  under the terms of the GNU General Public License as published by the
+--  Free Software Foundation; either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  Authors:
+--     Anthony <ajxs [at] panoptic.online>
+-------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------
+--  CXOS.ATA
+--
+--  Purpose:
+--    This package contains definitons and functionality for working with
+--    ATA devices.
+-------------------------------------------------------------------------------
+package Cxos.ATA is
+   pragma Preelaborate
+end Cxos.ATA;
+
